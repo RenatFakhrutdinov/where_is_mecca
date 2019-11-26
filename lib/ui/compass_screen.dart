3f16c3831eed739a22f08f7929fab6926c327f5a
@Fakhrutdinov <r.fakhrutdinov@galament.com>
2019-11-26 19:22:46 +0400

    check app for location data and connect

--- lib/ui/compass_screen.dart
+++ lib/ui/compass_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:where_is_mecca/bloc/location_bloc/location.dart';
 import 'package:where_is_mecca/localization/app_localizations.dart';
 
 class CompassScreen extends StatefulWidget {
@@ -7,16 +11,46 @@ class CompassScreen extends StatefulWidget {
 }
 
 class _CompassScreenState extends State<CompassScreen> {
+  Geolocator _geolocator = Geolocator();
+
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.of(context).title),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Where is Mecca?'),
-      ),
+      body: FutureBuilder(
+          future: _geolocator.isLocationServiceEnabled(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CupertinoActivityIndicator(),
+              );
+            } else
+              return Visibility(
+                visible: snapshot.data,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(height: ScreenUtil.getInstance().setHeight(200)),
+                    _compass(),
+                    Text(AppLocation.localName)
+                  ],
+                ),
+                replacement: Center(
+                    child: Text(AppLocalizations.of(context).noLocation,
+                        textAlign: TextAlign.center)),
+              );
+          }),
+    );
+  }
+
+  Widget _compass() {
+    return Center(
+      child: Text("Where is Mecca?"),
     );
   }
 }
